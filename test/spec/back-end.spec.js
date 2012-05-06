9cccd30d8acd9576df3faed1b9e977e41fd5246e
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-05-06 15:05:23 +0300

    Close MongoDB connection after test
    This solution is not yet ready, since the closing of the connection is
    called after every spec.

--- test/spec/back-end.spec.js
+++ test/spec/back-end.spec.js
@@ -1,11 +1,17 @@
 var database = require('../../lib/database')
+var jasmine = require('jasmine-node')
+
 describe('database', function() {
+  // How to add a callback into the finish function?
+  //console.log(this.queue.env.currentSuite.finish)
+
   beforeEach(function() {
     database.init('moodradiator-test')
   })
 
   afterEach(function() {
     database.dropAll()
+    database.disconnect()
   })
 
   it('allows persisting users', function() {
@@ -13,4 +19,5 @@ describe('database', function() {
     var user = new UserModel({nick: 'Wolfy'})
     user.save()
   })
+
 })
